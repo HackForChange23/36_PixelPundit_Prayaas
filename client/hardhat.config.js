b26cb0eb368864e194d3db5757ec801c2bde3718
Vishal Patil <vishalpatil6924@gmail.com>
2023-08-23 23:29:41 +0530

    added_sepolia_url

--- client/hardhat.config.js
+++ client/hardhat.config.js
@@ -2,9 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SEPOLIA_URL=process.env.SEPOLIA_URL;
-const PRIVATE_KEY=process.env.PRIVATE_KEY;
-
+const SEPOLIA_URL="https://sepolia.infura.io/v3/d923d1f93e4f4c24ba7fde6d81abc4ff";
+const PRIVATE_KEY="7fd22d5f5ffab0365bd07fc026a87f923713ac87b1e8afa3ee1c9e4d6454ad5f";
 
 module.exports = {
   solidity: "0.8.19",
