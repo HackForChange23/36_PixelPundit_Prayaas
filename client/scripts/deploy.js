576b72c5818feed771412f3b26aedb6bf27f9cf0
Vishal Patil <vishalpatil6924@gmail.com>
2023-08-23 23:40:52 +0530

    removed_test_folder

--- client/scripts/deploy.js
+++ client/scripts/deploy.js
@@ -1,8 +1,8 @@
 const hre=require("hardhat");
 
 async function main(){
-  const fundme =await hre.ethers.getContractFactory("Loan");
-  const contract =await fundme.deploy();
+  const fundme  = await hre.ethers.getContractFactory("Loan");
+  const contract = await fundme.deploy();
 
   await contract.deployed();
   console.log("ADDRESS OF CONTRACT:",contract.address)
