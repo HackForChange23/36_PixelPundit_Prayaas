5b4e8496cc532a623d5a9b593b202174c555af42
meetg21 <94597802+meetg21@users.noreply.github.com>
2023-08-23 21:26:29 +0530

    updated microcreditregister apply page

--- client/app/microcreditregister/page.jsx
+++ client/app/microcreditregister/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import AppBar from "@/components/AppBar";
 import React from "react";
 import Inputfield from "@/components/Inputfield";
@@ -5,11 +6,75 @@ import CenterCont from "@/components/CenterCont";
 import { HiCurrencyRupee } from "react-icons/hi";
 import Navbar from "@/components/Navbar";
 import "./microcredits.css";
+import { Input, Option } from "@material-tailwind/react";
+import { IoIosAddCircle } from "react-icons/io";
+import axios from "axios";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, {  } from "@mui/material/Select";
+import { Snackbar } from "@mui/material";
+import { useState } from "react";
+import {RiLockPasswordLine}  from "react-icons/ri";
+
 const register = () => {
+  const [members, setMembers] = useState([{ PrayaasId: "", amount: "" ,KCC: ""}]);
+  const [req, setReq] = useState("");
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+
+  const handleAddMember = () => {
+    if (members.length >= 3) {
+      setOpenSnackbar(true);
+      return;
+    }
+
+    const values = [...members];
+    values.push({ PrayaasId: "", amount: '',KCC: '' });
+    setMembers(values);
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
+  };
+
+  const handleMemberChange = (index, key, value) => {
+    const values = [...members];
+    values[index][key] = value;
+    setMembers(values);
+  };
+
+  const handleReqChange = (e) => {
+    setReq(e.target.value);
+  };
+
+  const calculateTotalValue = () => {
+    return members.reduce(
+      (total, member) => total + parseFloat(member.amount),
+      0
+    );
+  };
+
+  const sendData = () => {
+    const totalValue = calculateTotalValue();
+    const dataToSend = { members, totalValue,req };
+    // console.log(dataToSend);
+    axios
+      .post("YOUR_API_ENDPOINT", dataToSend)
+      .then((response) => {
+        console.log("Data sent successfully:", response.data);
+      })
+      .catch((error) => {
+        // Handle error
+        console.error("Error sending data:", error);
+      });
+  };
+
+  console.log(members);
   return (
     <>
       <AppBar />
-      <div className="min-h-screen flex flex-col relative overflow-hidden">
+      <div className="min-h-screen flex flex-col relative ">
         <div className="justify-center flex items-baseline credittitle">
           <div className="w-[75%] h-20 flex verify justify-center align-baseline font text-2xl text-black font-titleFont">
             <p className="credittitle flex flex-col justify-end">
@@ -22,58 +87,106 @@ const register = () => {
             Get Help Together
           </div>
         </div>
-        <div className="mt-4">
-          <CenterCont>
-            <div className="w-[75%]">
-              <label
-                for="first_name"
-                className="block mb-[1px] text-sm font-medium text-gray-900 dark:text-white"
+        <div className="w-[35%]">
+          <label
+            htmlFor="first_name"
+            className="block mb-[1px] text-xs font-medium text-gray-900"
+          ></label>
+          {/* <CenterCont> */}
+          <div className="flex justify-center items-center w-full  mt-5">
+            <FormControl sx={{ width: "400px", paddingX: "20px" }}>
+              <InputLabel id="demo-simple-select-label ml-3">
+                {" "}
+                Select Need
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={req}
+                label="Select Req"
+                onChange={handleReqChange}
+                sx={{
+                  width: "300px",
+                }}
               >
-                Loan needed
-              </label>
-              <div className="relative mb-6 w-auto ">
-                <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-                  <HiCurrencyRupee className="text-black" />
-                </div>
-                <input
-                  type="text"
-                  id="input-group-1"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
-                  placeholder="10000"
-                />
-              </div>
-            </div>
-          </CenterCont>
+                <MenuItem value={"Crop"}>Crop</MenuItem>
+                <MenuItem value={"Fertilizer"}>Fertilizer</MenuItem>
+                <MenuItem value={"Personal Needs"}>Personal Needs</MenuItem>
+                <MenuItem value={"Others"}>Others</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
 
-          <CenterCont>
-            <button
-              type="button"
-              className="bg-[#2B2A1E] h-10 rounded-2xl w-[75%] text-white"
-            >
-              Join Application
-            </button>
-          </CenterCont>
-          <CenterCont>
-            <div className="w-[80%] flex items-center justify-center relative py-2">
-              <div className="w-1/2 h-[1px] bg-black"></div>
-              <p className="text-lg text-black/80 bg-transparent p-3">OR</p>
-              <div className="w-1/2 h-[1px] bg-black"></div>
-            </div>
-          </CenterCont>
-          <CenterCont>
-            <button
-              type="button"
-              className="bg-[#2B2A1E] h-10 rounded-2xl w-[75%] text-white"
-            >
-              Create New
-            </button>
-          </CenterCont>
+          {/* </CenterCont> */}
         </div>
-        <Navbar></Navbar>
+        <CenterCont className="flex flex-">
+          <div className="mt-2 relative mb-2 w-[75%] ">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+              <RiLockPasswordLine className="text-black" />
+            </div>
+            <input
+              type="number"
+              id="input-group-1"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full pl-10 p-2.5"
+              placeholder="PIN"
+            />
+          </div>
+        </CenterCont>
+        <p className="flex items-center justify-center mt-3 font-semibold gap-2">
+          Add Team Members
+          <IoIosAddCircle
+            className="text-4xl text-green-500 "
+            onClick={handleAddMember}
+          />
+        </p>
+        <div className="">
+          {members.map((member, index) => (
+            <div key={index} className="flex flex-col mt-5">
+              <Inputfield
+                placeholder={`Team Member ${index + 1} Prayaas ID`} 
+                value={member.PrayaasId}
+                onChange={(e) =>{
+                  console.log(e.target.value);
+                  handleMemberChange(index, "PrayaasId", e.target.value)}
+                }
+              />
+              <Inputfield
+                placeholder={`Team Member ${index + 1} KCC`}
+                value={member.KCC}
+                onChange={(e) =>{
+                  console.log(e.target.value);
+                  handleMemberChange(index, "KCC", e.target.value)}
+                }
+                type="number"
+              />
+              <Inputfield
+                placeholder={`Team Member ${index + 1} Amount Required`}
+                value={member.amount}
+                onChange={(e) =>
+                  handleMemberChange(index, "amount", e.target.value)
+                }
+                type="number"
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="bg-[#7ACB2D] h-10 rounded-2xl w-[65%] ml-[60px]"
+          onClick={sendData}
+        >
+          Submit
+        </button>
       </div>
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={2000}
+        onClose={handleCloseSnackbar}
+        message="Cant add more than 3 members"
+        // action={action}
+      ></Snackbar>
 
-      {/* <Navbar><Navbar> */}
-
+      {/* <Navbar></Navbar> */}
     </>
   );
 };
